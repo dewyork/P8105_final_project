d3dd3365c03f2acd5967c76ad332b38c323365af
dewyork <at2710@cumc.columbia.edu>
2018-11-09 14:28:03 -0500

    Test upload

--- scratch_paper_dew.R
+++ scratch_paper_dew.R
@@ -1,22 +1,28 @@
 library(tidyverse)
 library(lubridate)
-incident_dat <- 
+#Original data from the website
+incident_dat <-  
   read_csv('data/Incidents_Responded_to_by_Fire_Companies.csv',
   col_types = "cicccicciccccccccccciccccccc")
+
+subset <- incident_dat %>% slice(1:100) %>% 
+  mutate(INCIDENT_DATE_TIME = mdy_hms(INCIDENT_DATE_TIME),
+        ARRIVAL_DATE_TIME = mdy_hms(ARRIVAL_DATE_TIME),
+        LAST_UNIT_CLEARED_DATE_TIME = mdy_hms(LAST_UNIT_CLEARED_DATE_TIME))
+
 incident_dat_2017 <- incident_dat %>% 
+  #recode date/time
   mutate(INCIDENT_DATE_TIME = mdy_hms(INCIDENT_DATE_TIME),
          ARRIVAL_DATE_TIME = mdy_hms(ARRIVAL_DATE_TIME),
          LAST_UNIT_CLEARED_DATE_TIME = mdy_hms(LAST_UNIT_CLEARED_DATE_TIME)) %>% 
-  filter(year(incident_dat$INCIDENT_DATE_TIME) == 2017)
+  #select year 2017
+  filter(year(INCIDENT_DATE_TIME) == 2017)
 
+#save 2017 data in data folder
+# save(incident_dat_2017, file = "data/incident_dat_2017.RData")
 
-# incident_dat <- 
-  # read_csv('data/Incidents_Responded_to_by_Fire_Companies.csv')
-subset <- incident_dat %>% slice(1:100)
-subset <- subset %>% 
-  mutate(INCIDENT_DATE_TIME = mdy_hms(INCIDENT_DATE_TIME),
-  ARRIVAL_DATE_TIME = mdy_hms(ARRIVAL_DATE_TIME),
-  LAST_UNIT_CLEARED_DATE_TIME = mdy_hms(LAST_UNIT_CLEARED_DATE_TIME))
+load('data/incident_dat_2017.Rdata')
 
+#create 
+incident_dat_2017 %>% ggplot(aes(x = yday(INCIDENT_DATE_TIME)))
 
-g <- mdy_hms(subset$INCIDENT_DATE_TIME)
