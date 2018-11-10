f3caeeccfdab658e0b46bb2d299d2a482ca0a079
dewyork <at2710@cumc.columbia.edu>
2018-11-09 21:44:56 -0500

    added more code

--- scratch_paper_dew.R
+++ scratch_paper_dew.R
@@ -23,6 +23,38 @@ incident_dat_2017 <- incident_dat %>%
 
 load('data/incident_dat_2017.Rdata')
 
-#create 
-incident_dat_2017 %>% ggplot(aes(x = yday(INCIDENT_DATE_TIME)))
+#create frequency by day for the whole year (x label as numeric)
+incident_dat_2017 %>% mutate(day = yday(INCIDENT_DATE_TIME)) %>% 
+  group_by(day) %>% count() %>% 
+  ggplot(aes(x = day, y = n)) + geom_line() + labs(y = 'Frequency')
+ # x label as date
+incident_dat_2017 %>% mutate(date = date(INCIDENT_DATE_TIME)) %>% 
+  group_by(date) %>% count() %>% 
+ggplot(aes(x = date, y = n)) + geom_line() + labs(y = 'Frequency')
+
+#Monthly trend
+incident_dat_2017 %>% mutate(month = month(INCIDENT_DATE_TIME)) %>% 
+  group_by(month) %>% count() %>% 
+  ggplot(aes(x = month, y = n)) + geom_line() + 
+  theme(axis.text.x = element_text(angle = 45)) +  labs(y = 'Frequency') +
+  scale_x_continuous(breaks = 1:12, labels = month.name)
+
+#By the hour
+incident_dat_2017 %>% mutate(hour = hour(INCIDENT_DATE_TIME)) %>% 
+  group_by(hour) %>% count() %>% 
+  ggplot(aes(x = hour, y = n)) + geom_line() + labs(y = 'Frequency')
+
+
+#Calculate lag time (response time)
+#And look at lag time (average) by the hour
+incident_dat_2017 %>% mutate(response_time = 
+    difftime(ARRIVAL_DATE_TIME, INCIDENT_DATE_TIME, units = 'mins'),
+    hour = hour(INCIDENT_DATE_TIME)) %>% 
+    group_by(hour) %>% 
+    summarise(mean_response_time = mean(response_time, na.rm = TRUE)) %>% 
+  ggplot(aes(x = hour, y = mean_response_time)) + geom_line() + 
+  labs(y = 'Mean response time (minutes)')
+  
+
+
 
